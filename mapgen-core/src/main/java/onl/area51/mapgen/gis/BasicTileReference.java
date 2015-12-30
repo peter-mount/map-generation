9813edf5b75d96e1474dd89657f7aa0e4d617603
peter <peter@europa>
2015-12-30 01:35:19 +0000

    OSM Tile to Coordinate conversions

---
+++ mapgen-core/src/main/java/onl/area51/mapgen/gis/BasicTileReference.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2015 peter.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package onl.area51.mapgen.gis;
+
+/**
+ * Implementation of {@link TileReference}
+ * <p>
+ * @author peter
+ */
+public class BasicTileReference
+        implements TileReference
+{
+
+    private final int z, x, y;
+    private final double xd, yd;
+
+    public BasicTileReference( int z, int x, int y )
+    {
+        this.z = z;
+        this.x = x;
+        this.y = y;
+        xd = x;
+        yd = y;
+    }
+
+    public BasicTileReference( int z, double xd, double yd )
+    {
+        this.z = z;
+        this.x = (int) Math.floor( xd );
+        this.y = (int) Math.floor( yd );
+        this.xd = xd;
+        this.yd = yd;
+    }
+
+    @Override
+    public int getZ()
+    {
+        return z;
+    }
+
+    @Override
+    public int getX()
+    {
+        return x;
+    }
+
+    @Override
+    public int getY()
+    {
+        return y;
+    }
+
+    @Override
+    public double getDecimalX()
+    {
+        return xd;
+    }
+
+    @Override
+    public double getDecimalY()
+    {
+        return yd;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 3;
+        hash = 53 * hash + this.z;
+        hash = 53 * hash + this.x;
+        hash = 53 * hash + this.y;
+        hash = 53 * hash + (int) (Double.doubleToLongBits( this.xd ) ^ (Double.doubleToLongBits( this.xd ) >>> 32));
+        hash = 53 * hash + (int) (Double.doubleToLongBits( this.yd ) ^ (Double.doubleToLongBits( this.yd ) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals( Object obj )
+    {
+        if( obj == null || getClass() != obj.getClass() ) {
+            return false;
+        }
+        final BasicTileReference other = (BasicTileReference) obj;
+        return this.z == other.z
+               && this.x == other.x
+               && this.y == other.y
+               && Double.doubleToLongBits( this.xd ) == Double.doubleToLongBits( other.xd )
+               && Double.doubleToLongBits( this.yd ) == Double.doubleToLongBits( other.yd );
+    }
+
+}
---
+++ mapgen-core/src/test/java/onl/area51/mapgen/gis/TileReferenceTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2015 peter.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package onl.area51.mapgen.gis;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import uk.trainwatch.gis.Coordinate;
+
+/**
+ *
+ * @author peter
+ */
+public class TileReferenceTest
+{
+
+    // Brandenburg Gate, Berlin
+    private static final double LONG = 13.37771496361961;
+    private static final double LAT = 52.51628011262304;
+    private static final int ZOOM = 17;
+    private static final int TILE_X = 70406;
+    private static final int TILE_Y = 42987;
+    private static final double TILE_Xd = 70406.67737697653;
+    private static final double TILE_Yd = 42987.96455135485;
+    private static final double delta = 0.01;
+
+    @Test
+    public void of()
+    {
+        TileReference r = TileReference.of( ZOOM, TILE_X, TILE_Y );
+        assertEquals( ZOOM, r.getZ() );
+        assertEquals( TILE_X, r.getX() );
+        assertEquals( TILE_Y, r.getY() );
+    }
+
+    @Test
+    public void fromCoordinate()
+    {
+        Coordinate c = Coordinate.of( LONG, LAT );
+
+        TileReference r = TileReference.fromCoordinate( ZOOM, c );
+        assertEquals( ZOOM, r.getZ() );
+        assertEquals( TILE_X, r.getX() );
+        assertEquals( TILE_Y, r.getY() );
+        assertEquals( TILE_Xd, r.getDecimalX(), delta );
+        assertEquals( TILE_Yd, r.getDecimalY(), delta );
+    }
+
+    @Test
+    public void toCoordinate()
+    {
+        TileReference r = TileReference.of( ZOOM, TILE_X, TILE_Y );
+        Coordinate c = r.toCoordinate();
+        assertEquals( LONG, c.getLongitude(), delta );
+        assertEquals( LAT, c.getLatitude(), delta );
+    }
+}
